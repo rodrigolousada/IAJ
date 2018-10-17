6c280eab617c6d8951a814f627a8825ced446797
rodrigolousada <rodrigo.m.lousada@gmail.com>
2018-10-17 16:48:44 +0100

    Starting Checkpoint 2
    Done:
    * Initial checkpoint files
    * Checkpoitns Assignment files
    
    To be done:
    * Discussion with professor about Project 1
    * Starting Checkpoint 2\

---
+++ Checkpoint2/Assets/Scripts/IAJ.Unity/Pathfinding/Path/GlobalPath.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.IAJ.Unity.Utils;
+using RAIN.Navigation.Graph;
+using UnityEngine;
+using System;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.Path
+{
+    public class GlobalPath : Path
+    {
+        public List<NavigationGraphNode> PathNodes { get; protected set; }
+        public List<Vector3> PathPositions { get; protected set; } 
+        public bool IsPartial { get; set; }
+        public float Length { get; set; }
+        public List<LocalPath> LocalPaths { get; protected set; } 
+
+
+        public GlobalPath()
+        {
+            this.PathNodes = new List<NavigationGraphNode>();
+            this.PathPositions = new List<Vector3>();
+            this.LocalPaths = new List<LocalPath>();
+        }
+
+        
+
+        public override float GetParam(Vector3 position, float previousParam)
+        {
+			//TODO: implement latter
+			throw new NotImplementedException();
+        }
+
+        public override Vector3 GetPosition(float param)
+        {
+			//TODO: implement latter
+			throw new NotImplementedException();
+        }
+
+        public override bool PathEnd(float param)
+        {
+			//TODO: implement latter
+			throw new NotImplementedException();
+        }
+    }
+}
